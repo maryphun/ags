2e34f4bb102e2af22fbcba7aad421eb0ea2eafc6
MARY <phunmung@gmail.com>
2019-10-29 20:22:12 +0900

    10/29

--- Assets/Scripts/Menu.cs
+++ Assets/Scripts/Menu.cs
@@ -5,25 +5,6 @@ using UdpKit;
 
 public class Menu : Bolt.GlobalEventListener
 {
-    //public void OnGUI()
-    //{
-    //    GUILayout.BeginArea(new Rect(10, 10, Screen.width - 20, Screen.height - 20));
-
-    //    if (GUILayout.Button("Start Server", GUILayout.ExpandWidth(true), GUILayout.ExpandHeight(true)))
-    //    {
-    //        // START SERVER
-    //        BoltLauncher.StartServer();
-    //    }
-
-    //    if (GUILayout.Button("Start Client", GUILayout.ExpandWidth(true), GUILayout.ExpandHeight(true)))
-    //    {
-    //        // START CLIENT
-    //        BoltLauncher.StartClient();
-    //    }
-
-    //    GUILayout.EndArea();
-    //}
-
     public void StartSeerver()
     {
         // サーバーを立てる
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,48 +4,58 @@ using UnityEngine;
 
 public class Player : Bolt.EntityBehaviour<ICubeStateCustom>
 {
+    private Camera camera;
+    private CharacterController characterController;
+    private Animator animator;
+    private bool shot;
     //start()
     public override void Attached()
     {
+        //network
         state.SetTransforms(state.CubeTransform, transform);
+        
 
-        if (entity.IsOwner)
-        {
-            state.Color = new Color(Random.value, Random.value, Random.value);
-        }
+        //if (entity.IsOwner)
+        //{
+        //}
 
-        state.AddCallback("Color", ColorChanged);
+        //player
+        characterController = GetComponent<CharacterController>();
+        animator = GetComponent<Animator>();
+        camera = FindObjectOfType<Camera>();
     }
-
-    void OnGUI()
-    {
-        if (entity.IsOwner)
-        {
-            GUI.color = state.Color;
-            GUILayout.Label("@@@");
-            GUI.color = Color.white;
-        }
-    }
-
+    
     //update()
     public override void SimulateOwner()
     {
-        var speed = 4f;
+        var speed = 5f;
         var movement = Vector3.zero;
+        shot = Input.GetMouseButton(0);
+        state.Shot = shot;
 
-        if (Input.GetKey(KeyCode.W)) { movement.z += 1; }
-        if (Input.GetKey(KeyCode.S)) { movement.z -= 1; }
-        if (Input.GetKey(KeyCode.A)) { movement.x -= 1; }
-        if (Input.GetKey(KeyCode.D)) { movement.x += 1; }
+        if (Input.GetKey(KeyCode.W)) { movement.z += speed; }
+        if (Input.GetKey(KeyCode.S)) { movement.z -= speed; }
+        if (Input.GetKey(KeyCode.A)) { movement.x -= speed; }
+        if (Input.GetKey(KeyCode.D)) { movement.x += speed; }
+
+        Ray cameraRay = camera.ScreenPointToRay(Input.mousePosition);
+        Plane groundPlane = new Plane(Vector3.up, Vector3.zero);
+        float rayLength;
+
+        if (groundPlane.Raycast(cameraRay, out rayLength))
+        {
+            Vector3 pointToLook = cameraRay.GetPoint(rayLength);
+            Debug.DrawLine(cameraRay.origin, pointToLook, Color.red);
+
+            transform.LookAt(pointToLook);
+        }
 
         if (movement != Vector3.zero)
         {
             transform.position = transform.position + (movement.normalized * speed * BoltNetwork.FrameDeltaTime);
         }
-    }
 
-    void ColorChanged()
-    {
-        GetComponent<Renderer>().material.color = state.Color;
+        animator.SetBool("Run", movement != Vector3.zero);
+        animator.SetBool("Shot", shot);
     }
 }
